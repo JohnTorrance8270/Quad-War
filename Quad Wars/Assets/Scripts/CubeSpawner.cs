f7ad83f66b5b6f9ac5ff2999adec58428219f615
JohnTorrance8270 <johnabertay8270@hotmail.com>
2018-08-21 14:33:06 +0100

    Dictonaries and object pool development
    Dictonaries and object pool development

---
+++ Quad Wars/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+
+	// Use this for initialization
+	void Start ()
+    {
+        objectPooler = ObjectPooler.Instance;
+	}
+	
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        objectPooler.SpawnFromPool("Cube", transform.position, Quaternion.identity);
+    }
+}
